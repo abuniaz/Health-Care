2a6e4bb1e09a0d3b758a586fe436dc8ea073439f
Abu Niaz <niaz35-507@diu.edu.bd>
2022-02-10 13:15:22 +0600

    Update home.dart

--- health_care/lib/Sign_Up/home.dart
+++ health_care/lib/Sign_Up/home.dart
@@ -10,84 +10,85 @@ class MainSignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: const Text(
-        "Sign Up",
-        style: TextStyle(
-          color: Colors.black,
-        ),
-      )),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding:
-              const EdgeInsets.only(left: 15, right: 15, top: 8, bottom: 8),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: "First Name"),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: "Last Name"),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(labelText: "Phone Number"),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(labelText: "Email Address"),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: "Sex"),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(labelText: "Old"),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: "Password"),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                decoration:
-                    const InputDecoration(labelText: "Confirm Password"),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              BottonWidget(
-                  text: "Sign Up",
-                  press: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => const Body1()));
-                  }),
-            ],
+        appBar: AppBar(
+            title: const Text(
+          "Sign Up",
+          style: TextStyle(
+            color: Colors.black,
           ),
-        ),
-      ),
-    );
+        )),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding:
+                const EdgeInsets.only(left: 15, right: 15, top: 8, bottom: 8),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: "First Name"),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: "Last Name"),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(labelText: "Phone Number"),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(labelText: "Email Address"),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: "Sex"),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(labelText: "Old"),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: "Password"),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration:
+                      const InputDecoration(labelText: "Confirm Password"),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                BottonWidget(
+                    text: "Sign Up",
+                    press: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Body1()));
+                    }),
+              ],
+            ),
+          ),
+        ));
   }
 }
