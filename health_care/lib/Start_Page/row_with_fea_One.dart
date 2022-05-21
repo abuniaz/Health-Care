58c6cb751b620030af63187bba8d7730c59dac46
Abu Niaz <niaz35-507@diu.edu.bd>
2022-05-21 19:56:35 +0600

    Update row_with_fea_One.dart

--- health_care/lib/Start_Page/row_with_fea_One.dart
+++ health_care/lib/Start_Page/row_with_fea_One.dart
@@ -11,18 +11,21 @@ class RowWithFeatureOne extends StatelessWidget {
       children: [
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: const [
+          children: [
             Feature(
               name: '    Seat\n Booking',
               img: 'images/bed.png',
+              press: () {},
             ),
             Feature(
               name: 'Medicine',
               img: 'images/medi2.jpg',
+              press: () {},
             ),
             Feature(
               name: 'Dental',
               img: 'images/den.jpg',
+              press: () {},
             )
           ],
         ),
@@ -31,18 +34,21 @@ class RowWithFeatureOne extends StatelessWidget {
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: const [
+          children: [
             Feature(
               name: '    Seat\n Booking',
               img: 'images/bed.png',
+              press: () {},
             ),
             Feature(
               name: 'Medicine',
               img: 'images/medi2.jpg',
+              press: () {},
             ),
             Feature(
               name: 'Dental',
               img: 'images/den.jpg',
+              press: () {},
             )
           ],
         ),
@@ -51,14 +57,16 @@ class RowWithFeatureOne extends StatelessWidget {
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: const [
+          children: [
             Feature(
               name: '    Seat\n Booking',
               img: 'images/bed.png',
+              press: () {},
             ),
             Feature(
               name: 'Medicine',
               img: 'images/medi2.jpg',
+              press: () {},
             ),
           ],
         ),
@@ -68,39 +76,45 @@ class RowWithFeatureOne extends StatelessWidget {
 }
 
 class Feature extends StatelessWidget {
-  const Feature({
+  Feature({
     Key? key,
     required this.name,
     required this.img,
+    required this.press,
   }) : super(key: key);
   final String name;
   final String img;
+
+  void Function() press;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 80,
-          width: 80,
-          decoration: BoxDecoration(
-            color: Colors.red,
-            borderRadius: BorderRadius.circular(25),
-            image: DecorationImage(
-                image: AssetImage(
-                  img,
-                ),
-                fit: BoxFit.cover),
+    return GestureDetector(
+      onTap: press,
+      child: Column(
+        children: [
+          Container(
+            height: 80,
+            width: 80,
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(25),
+              image: DecorationImage(
+                  image: AssetImage(
+                    img,
+                  ),
+                  fit: BoxFit.cover),
+            ),
           ),
-        ),
-        const SizedBox(
-          height: 4,
-        ),
-        Text(
-          name,
-          style:
-              const TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
-        )
-      ],
+          const SizedBox(
+            height: 4,
+          ),
+          Text(
+            name,
+            style: const TextStyle(
+                fontWeight: FontWeight.bold, color: Colors.black38),
+          )
+        ],
+      ),
     );
   }
 }
